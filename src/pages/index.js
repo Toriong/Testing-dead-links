99f5332cf4590f5a7da2e804a4523be659714548
Toriong <gtorion97@gmail.com>
2024-12-02 14:25:02 -0800

    testing dead links check fail

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
               Don{"'"}t click me!
             </span>
           </button>
-          <Link href="https://csp-example-rho.vercel.app/hi">
+          <Link href="https://csp-example-rho.vercel.app/hilsadjf">
             sup there
           </Link>
           <p>
