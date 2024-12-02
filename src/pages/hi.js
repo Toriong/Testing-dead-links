ef92f8b346c13e147ce766e03c08b8498fb9b9b3
Toriong <gtorion97@gmail.com>
2024-12-02 11:21:45 -0800

    added logs

--- src/pages/hi.js
+++ src/pages/hi.js
@@ -22,6 +22,10 @@ const HiPage = ({ data }) => {
 export const getStaticProps = async (arg) => {
     const { MONGODB_PASSWORD, MONGODB_USER, MONGODB_DB_NAME, MONGODB_DB_PROD } =
         process.env;
+
+    console.log("process.env: ", process.env);
+    console.log('MONGODB_DB_NAME, yo there: ', MONGODB_DB_NAME);
+
     let dbName = MONGODB_DB_NAME;
 
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
