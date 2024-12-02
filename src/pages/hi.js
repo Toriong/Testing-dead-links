77449ca3cea47c27aab8b169c10a4cec0f0ef382
Toriong <gtorion97@gmail.com>
2024-12-02 12:56:06 -0800

    test

--- src/pages/hi.js
+++ src/pages/hi.js
@@ -19,26 +19,28 @@ const HiPage = ({ data }) => {
 };
 
 export const getStaticProps = async (arg) => {
-
-    const { MONGODB_PASSWORD, MONGODB_USER, MONGODB_DB_NAME, MONGODB_DB_PROD } =
-        process.env ?? {};
+    const {
+        NEXT_PUBLIC_MONGODB_PASSWORD,
+        NEXT_PUBLIC_MONGODB_USER,
+        NEXT_PUBLIC_MONGODB_DB_NAME,
+        NEXT_PUBLIC_MONGODB_DB_PROD
+    } = process.env ?? {};
 
     console.log("process.env: ", process.env);
-    console.log("MONGODB_DB_NAME, yo there bacon: ", MONGODB_DB_NAME);
-    console.log("yo there secrets.MONGODB_DB_NAME, sup there: ", secrets);
+    console.log("NEXT_PUBLIC_MONGODB_DB_NAME, yo there bacon: ", NEXT_PUBLIC_MONGODB_DB_NAME);
+    console.log("yo there secrets.NEXT_PUBLIC_MONGODB_DB_NAME, sup there: ", secrets);
 
-    let dbName = MONGODB_DB_NAME;
+    let dbName = NEXT_PUBLIC_MONGODB_DB_NAME;
 
     if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
-        dbName = MONGODB_DB_PROD;
+        dbName = NEXT_PUBLIC_MONGODB_DB_PROD;
     }
 
-    const connectionUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.tynope2.mongodb.net/${dbName}`;
+    const connectionUri = `mongodb+srv://${NEXT_PUBLIC_MONGODB_USER}:${NEXT_PUBLIC_MONGODB_PASSWORD}@cluster0.tynope2.mongodb.net/${dbName}`;
     const connectionState = await mongoose.connect(connectionUri, {
         retryWrites: true,
     });
 
-    console.log("yo there secrets: ", secrets.MONGODB_DB_NAME);
     console.log("connectionState result: ", connectionState);
 
     return {
