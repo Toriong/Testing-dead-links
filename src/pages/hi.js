0b09277f1e1c344a87882c2a54ea938a2c25a694
Toriong <gtorion97@gmail.com>
2024-12-02 10:57:12 -0800

    forcing a fail for dead links check

--- src/pages/hi.js
+++ src/pages/hi.js
@@ -1,13 +1,12 @@
 import { connectToMongodb } from "@/connection";
+import mongoose from "mongoose";
 
 const { useEffect } = require("react");
 
 const HiPage = ({ data }) => {
-
-
     useEffect(() => {
         (async () => {
-            const res = await fetch('/api/hello');
+            const res = await fetch("/api/hello");
             const data = await res.json();
             console.log("yo there data: ", data);
         })();
@@ -18,18 +17,29 @@ const HiPage = ({ data }) => {
             <h1>Hi</h1>
         </>
     );
-}
+};
 
 export const getStaticProps = async (arg) => {
-    await connectToMongodb();
+    const { MONGODB_PASSWORD, MONGODB_USER, MONGODB_DB_NAME, MONGODB_DB_PROD } =
+        process.env;
+    let dbName = MONGODB_DB_NAME;
 
-    console.log("Connection to database has been made.");
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+        dbName = MONGODB_DB_PROD;
+    }
+
+    const connectionUri = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.tynope2.mongodb.net/${dbName}`;
+    const connectionState = await mongoose.connect(connectionUri, {
+        retryWrites: true,
+    });
+
+    console.log("connectionState: ", connectionState);
+
     return {
         props: {
-            data: "hello"
-        }
-    }
-}
+            data: "hello",
+        },
+    };
+};
 
 export default HiPage;
-
